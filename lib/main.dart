2e1d35941997337f8494c50b464fbe6db690f3b7
Richard <grnganon@gmail.com>
2021-12-30 14:54:26 -0500

    More layout and adding questions example

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,34 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'How old are you?',
+      'Do you prefer dessert or entrees?'
+    ];
+    // var questionWidgetList = <Widget>[];
+    // questions.forEach((question) => questionWidgetList.add(Text(question)));
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
           backgroundColor: Color(0xFF5DBB63),
         ),
-        body: Text('Here\'s some default text!'),
+        body: Column(children: [
+          Text('Example Question'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: () => {},
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => {},
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () => {},
+          )
+        ]),
       ),
     );
   }

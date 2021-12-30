c4bbf05f64248f6579124a74c0468306e25dfa24
Richard <grnganon@gmail.com>
2021-12-30 14:36:47 -0500

    Added scaffold and some basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hellow!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Color(0xFF5DBB63),
+        ),
+        body: Text('Here\'s some default text!'),
+      ),
     );
   }
 }
